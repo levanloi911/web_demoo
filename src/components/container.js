2dc306b5865158fda02647748e8ac596cc5d8710
levanloi911 <54982847+levanloi911@users.noreply.github.com>
2021-07-22 16:57:28 +0700

    ok

--- src/components/container.js
+++ src/components/container.js
@@ -1,7 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import './container.css'
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css"
+import './container.css'
+
 
 // import Swiper core and required modules
 import SwiperCore, {
@@ -14,27 +15,32 @@ SwiperCore.use([Navigation, Autoplay]);
 
 function Container(){
     return(
-            <div className='container_swiper'>
-                <div style={{width:'70%', display:'inline-block'}}>
-                    <div style={{float:'left', width:'70%', height:''}}>
+            <div className='container_'>
+                <div className='container_swiper' >
+                    <div className='box_left'>
                         <Swiper navigation={true} autoplay={true} className="my_Swiper" >
                             <SwiperSlide className='SwiperSlide'><Image src='https://suachualaptop24h.com/images/slideshow/2020/07/20/compress2/suachualaptop24h-tantam-uytin-chuyennghiep_1595238883.jpg.webp'/></SwiperSlide>
                             <SwiperSlide className='SwiperSlide'><Image src='https://suachualaptop24h.com/images/slideshow/2020/07/20/compress2/suachualaptop24h-tantam-uytin-chuyennghiep_1595238883.jpg.webp'/></SwiperSlide>
                             <SwiperSlide className='SwiperSlide'><Image src='https://suachualaptop24h.com/images/slideshow/2020/07/20/compress2/suachualaptop24h-tantam-uytin-chuyennghiep_1595238883.jpg.webp'/></SwiperSlide>
                         </Swiper>
                     
-                        <div style={{float:'left', width:'20%', textAlign:'center',padding:'10px', borderRadius:'5px', borderColor:'red', border:'2px  solid red'}}>	
-						Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</div>
-                        <div style={{float:'left', width:'20%', textAlign:'center',padding:'10px', borderRadius:'5px', borderColor:'red', border:'2px  solid red'}}>	
-						Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</div>
-                        <div style={{float:'left', width:'20%', textAlign:'center',padding:'10px', borderRadius:'5px', borderColor:'red', border:'2px  solid red'}}>	
-						Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</div>
-                        <div style={{float:'left', width:'20%', textAlign:'center',padding:'10px', borderRadius:'5px', borderColor:'red', border:'2px  solid red'}}>	
-						Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</div>
+                        <div className='container_content'>
+                            <p>1Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</p>
+                            <p>2Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</p>
+                            <p>2Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</p>
+
+                            <p>2Dịch vụ tại nhà và Văn phòng nhanh chóng, chuyên nghiệp</p>
+
+                            
+                            
+                        </div>
+                       
+
+                     
                                
                     </div>
                 
-                <div className='image' style={{float:'right', width:'27%', marginLeft:'10px'}}>
+                <div className='box_right'>
                 <Image src='https://suachualaptop24h.com/images/banners/compress/sua-laptop-tan-nha_1623750246.jpeg.webp'/>
                 <Image src='https://suachualaptop24h.com/images/banners/compress/sua-laptop-tan-nha_1623750246.jpeg.webp'/>
                 </div>
